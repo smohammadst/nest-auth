4bc6484714876a09090f68b47e743beec90586cd
smohammadst <140355232+smohammadst@users.noreply.github.com>
2024-03-06 04:27:25 -0800

    add project

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,26 @@
+import {Body, Controller, Post} from "@nestjs/common";
+import {AuthService} from "./auth.service";
+import {CheckOtpDto, SendOtpDto} from "./dto/otp.dto";
+import {LoginDto, SignupDto} from "./dto/basic.dto";
+
+@Controller("auth")
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post("/send-otp")
+  sendOtp(@Body() otpDto: SendOtpDto) {
+    return this.authService.sendOtp(otpDto);
+  }
+  @Post("/check-otp")
+  checkOtp(@Body() otpDto: CheckOtpDto) {
+    return this.authService.checkOtp(otpDto);
+  }
+  @Post("/signup")
+  signup(@Body() signupDto: SignupDto) {
+    return this.authService.signup(signupDto);
+  }
+  @Post("/login")
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
---
+++ src/modules/user/entities/user.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import {OTPEntity} from "./otp.entity";
+
+@Entity("user")
+export class UserEntity {
+  @PrimaryGeneratedColumn("increment")
+  id: number;
+  @Column({nullable: true})
+  first_name: string;
+  @Column({nullable: true})
+  last_name: string;
+  @Column({nullable: true})
+  email: string;
+  @Column({nullable: true})
+  password: string;
+  @Column({nullable: true})
+  mobile: string;
+  @Column({default: false})
+  mobile_verify: boolean;
+  @CreateDateColumn()
+  created_at: Date;
+  @UpdateDateColumn()
+  updated_at: Date;
+  @Column({nullable: true})
+  otpId: number;
+  @OneToOne(() => OTPEntity, (otp) => otp.user)
+  @JoinColumn({name: "otpId"})
+  otp: OTPEntity;
+}
